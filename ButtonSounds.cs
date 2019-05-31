6c4d119a8a0c4442e1f542553d3bb7851a6c1dda
Watson121 <33036071+Watson121@users.noreply.github.com>
2019-05-31 09:54:39 +0200

    Add files via upload

---
+++ ButtonSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour {
+
+    public AudioSource myFx;
+    public AudioClip hoverFX;
+    public AudioClip clickFx;
+
+    public void PlaySoundWhenHover()
+    {
+        myFx.PlayOneShot(hoverFX);
+    }
+
+    public void PlaySoundWhenClicked()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        StartCoroutine(DoAfter());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+    IEnumerator DoAfter ()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+
+}
